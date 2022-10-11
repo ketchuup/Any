86424eb6bb6168bc531bd4c4bd089ae426ace819
ketchuup <antonYmanin@gmail.com>
2022-10-12 01:56:09 +0300

    What?

--- Renderer.py
+++ Renderer.py
@@ -2,13 +2,15 @@ import numpy
 import Colors
 import PIL, PIL.ImageDraw, PIL.ImageFont
 
-size = (_, height) = 32, 32
-font = PIL.ImageFont.truetype('Fonts/FiraCode.ttf', 16)
+size = (_, height) = 64, 64
+font = PIL.ImageFont.truetype('Fonts/FiraCode.ttf', 32)
 
 def render(character: str) -> numpy.ndarray:
     image = PIL.Image.new('1', size, Colors.white)
 
     canvas = PIL.ImageDraw.Draw(image)
     canvas.text((0, 0), character, Colors.black, font)
+
+    image.save(f'{character}.png')
 
     return numpy.array(image.getdata()).reshape(size[::-1])
