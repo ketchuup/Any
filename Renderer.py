f0b6c78a95986bbf6d8df878db9f3f81ff13ab4b
ketchuup <antonYmanin@gmail.com>
2022-10-12 01:22:09 +0300

    Increase font size

--- Renderer.py
+++ Renderer.py
@@ -3,12 +3,14 @@ import Colors
 import PIL, PIL.ImageDraw, PIL.ImageFont
 
 size = (_, height) = 32, 32
-font = PIL.ImageFont.truetype('Fonts/FiraCode.ttf', 8)
+font = PIL.ImageFont.truetype('Fonts/FiraCode.ttf', 16)
 
 def render(character: str) -> numpy.ndarray:
     image = PIL.Image.new('1', size, Colors.white)
 
     canvas = PIL.ImageDraw.Draw(image)
     canvas.text((0, 0), character, Colors.black, font)
+
+    image.save(f'{character}.png')
 
     return numpy.array(image.getdata()).reshape(size[::-1])
